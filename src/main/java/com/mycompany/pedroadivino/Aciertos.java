3dbb08819762249c2607c1fc8b6f00ef526956c4
Usuario <Usuario@DESKTOP-UNSG7D8>
2019-10-17 10:24:01 +0200

    Primera version

---
+++ src/main/java/com/mycompany/pedroadivino/Aciertos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pedroadivino;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aciertos {
+    
+    public static void main(String[] args) {
+        int n = 0,cont = 0;
+        int x = (int) (500 *Math.random());
+        //System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+}
