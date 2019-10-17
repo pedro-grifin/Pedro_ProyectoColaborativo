447542c3e026b461154a1223f2aff31a538f4071
Usuario <Usuario@DESKTOP-UNSG7D8>
2019-10-17 10:36:19 +0200

    Modificacion para numeros de 10 al 50

--- src/main/java/com/mycompany/pedroadivino/Aciertos.java
+++ src/main/java/com/mycompany/pedroadivino/Aciertos.java
@@ -15,7 +15,7 @@ public class Aciertos {
     
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) ((Math.random()*(50-10)+10));
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
